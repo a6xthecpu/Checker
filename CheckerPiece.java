108d5b07cc8f4041d6b37660f456dc04152c1b4f
hammyexe <89997092+hammyexe@users.noreply.github.com>
2025-01-26 11:33:43 +0300

    Update CheckerPiece.java

--- CheckerPiece.java
+++ CheckerPiece.java
@@ -1,9 +1,19 @@
 public class CheckerPiece {
     private final String PIECE;
+    private int row, col;
 
-    public CheckerPiece(String piece) {
+    public CheckerPiece(String piece, int row, int col) {
         PIECE = piece;
+        this.row = row;
+        this.col = col
     }
+
+    public int getRow(){
+        return row
+    }
+    public int getCol(){
+        return col
+            }
 
 
 
